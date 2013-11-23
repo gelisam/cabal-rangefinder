51a2c01118c4e9a36a515365fb8c630aaebda671
Samuel Gélineau <gelisam+github@gmail.com>
2013-11-23 17:09:30 -0500

    use the Cabal library (fails)
    We can parse the cabal file, but unfortunately the whitespace is lost
    when we write it back.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import Distribution.PackageDescription
+import Distribution.PackageDescription.Parse
+import Distribution.Verbosity
+import System.Environment
+
 
 main :: IO ()
-main = return ()
+main = do
+    [cabal_file] <- getArgs
+    d <- readPackageDescription normal cabal_file
+    writePackageDescription cabal_file (packageDescription d)
