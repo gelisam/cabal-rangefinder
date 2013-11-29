dbf08080f44eb04c134e5f914d8ea7724400fd03
Samuel Gélineau <gelisam+github@gmail.com>
2013-11-28 23:15:17 -0500

    the stack thickens
    the parser is now using a more complicated monad transformer, which
    allows the actual parsing code to be simplified. Still not shorted than
    the original mess, but much more readable!

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Applicative
 import Control.Arrow
+import Control.Monad.State
 import Control.Monad.Writer
 import Data.Functor
 import Data.List
@@ -17,150 +18,175 @@ import Text.PrettyPrint hiding (empty)
 import Text.Printf
 
 
--- unlike Distribution.PackageDescription, we keep the whitespace.
+-- Unlike Distribution.PackageDescription, we keep the whitespace.
 type Cabal = [Either String Dependency]
 
 -- | Parse a .cabal file by writing it piece by piece.
-type ParseC a = WriterT Cabal Maybe a
+type ParseC a = StateT String (WriterT Cabal Maybe) a
 
 -- | Print the parsed pieces, simplifies tests.
-testC :: Show a => ParseC a -> IO ()
-testC = go . runWriterT
+testC :: Show a => String -> ParseC a -> IO ()
+testC s = go . runWriterT . (`runStateT` s)
   where
     go Nothing = putStrLn "Nothing"
-    go (Just (x, ys)) = do
+    go (Just ((x, s), ys)) = do
       mapM_ putStrLn $ map show' ys
       print x
+      unless (null s) $ print s
     show' (Left s) = show s
     show' (Right d) = printf "[%s]" $ render $ disp d
 
--- | Write a String, which we won't modify further.
--- >>> testC $ stringC "foo"
+
+splitConsume :: (Functor m, Monad m)
+          => (s -> (e, s))
+          -> (e -> m a)
+          -> StateT s m a
+splitConsume split consume = do
+    (e, s) <- split <$> get
+    put s
+    lift $ consume e
+
+splitXform :: (String -> (String, String)) -> (String -> a) -> ParseC a
+splitXform split f = splitConsume split go
+  where
+    go s = do
+        tell [Left s]
+        return $ f s
+
+splitIgnore :: (String -> (String, String)) -> ParseC ()
+splitIgnore = (`splitXform` const ())
+
+splitWhitespace :: String -> (String, String)
+splitWhitespace = span (`elem` " \t")
+
+
+-- | Consume the string if it matches the head of the input stream.
+-- >>> testC "fool" $ stringC "foo"
 -- "foo"
 -- ()
+-- "l"
+-- 
+-- >>> testC "bard" $ stringC "foo"
+-- Nothing
+-- 
+-- >>> testC "bard" $ stringC "foo" <|> stringC "bar"
+-- "bar"
+-- ()
+-- "d"
 stringC :: String -> ParseC ()
-stringC s = tell [Left s]
+stringC expected = splitConsume split check
+  where
+    split = splitAt $ length expected
+    check actual = do
+        guard $ actual == expected
+        tell [Left actual]
 
--- | Parse and write a Dependency, which we will modify later.
--- >>> testC $ dependencyC "containers"
+-- | Consume a dependency if it matches the head of the input stream.
+-- >>> testC "containers" dependencyC
 -- [containers -any]
 -- ()
 -- 
--- >>> testC $ dependencyC "bogus text"
+-- >>> testC "containers, mtl" dependencyC
+-- [containers -any]
+-- ()
+-- ", mtl"
+-- 
+-- >>> testC "bogus text" dependencyC
 -- Nothing
-dependencyC :: String -> ParseC ()
-dependencyC s = do
-    d <- lift $ simpleParse s
-    tell [Right d]
+dependencyC :: ParseC ()
+dependencyC = splitConsume split check
+  where
+    split = break (`elem` ",\n")
+    check s = do
+        d <- lift $ simpleParse s
+        tell [Right d]
 
 
--- | In general, a fooC function performs foo in the ParseC monad,
---   but also writes whatever was consumed.
-
--- | Change, but also write fst.
--- >>> testC $ mapFstC length ("foo", 42)
--- "foo"
--- (3,42)
-mapFstC :: (String -> a) -> (String, b) -> ParseC (a, b)
-mapFstC f (s, x) = stringC s >> return (f s, x)
-
--- | Like snd, but also write fst.
--- >>> testC $ sndC ("foo",42)
--- "foo"
--- 42
-sndC :: (String, a) -> ParseC a
-sndC = fmap snd . mapFstC id
-
--- | Like dropWhile, but also write the dropped part.
-dropWhileC :: (Char -> Bool) -> String -> ParseC String
-dropWhileC p = sndC . span p
-
-dropWhitespaceC :: String -> ParseC String
-dropWhitespaceC = dropWhileC (`elem` " \t")
-
--- | Like isPrefixOf, but also write the prefix and return the remainder.
--- >>> testC $ "abc" `isPrefixOfC` "abcdef"
--- "abc"
--- "def"
+-- | Only succeed if the file has been completely consumed.
+-- >>> testC "" eofC
+-- ()
 -- 
--- >>> testC $ "abc" `isPrefixOfC` "ABCDEF"
+-- >>> testC "more" eofC
 -- Nothing
-isPrefixOfC :: String -> String -> ParseC String
-isPrefixOfC prefix s = do
-    guard $ prefix `isPrefixOf` s
-    stringC prefix
-    return $ drop (length prefix) s
+eofC :: ParseC ()
+eofC = do
+    s <- get
+    guard $ null s
+
+-- | Only succeed if the line has been completely consumed.
+-- >>> testC "\n" eolC
+-- "\n"
+-- ()
+-- 
+-- >>> testC "more\n" eolC
+-- Nothing
+eolC :: ParseC ()
+eolC = stringC "\n"
 
 
--- | Write and measure the indentation.
--- 
--- >>> fst $ splitIndent "  hello"
--- 2
--- 
--- >>> testC $ snd $ splitIndent "  hello"
+-- | Consume and ignore whitespace.
+-- >>> testC "  hello" whitespaceC
 -- "  "
+-- ()
 -- "hello"
-splitIndent :: String -> (Int, ParseC String)
-splitIndent = go . span (`elem` " \t")
-  where
-    go (i, s) = (length i, stringC i >> return s)
+whitespaceC :: ParseC ()
+whitespaceC = splitIgnore splitWhitespace
 
--- | Extract an indented block, indented by more than i chars.
+-- | Consume and ignore the rest of the line.
+-- >>> testC "hello" lineC
+-- "hello"
+-- ()
 -- 
--- >>> splitUnindent 2 [(4,"A"),(4,"B"),(6,"B1"),(6,"B2"),(4,"C"),(2,"else")]
--- (["A","B","B1","B2","C"],[(2,"else")])
-splitUnindent :: Int -> [(Int, a)] -> ([a], [(Int, a)])
-splitUnindent i = (map snd *** id) . span isMoreIndented
-  where
-    isMoreIndented = (> i) . fst
+-- >>> testC "hello\nworld\n" lineC
+-- "hello"
+-- "\n"
+-- ()
+-- "world\n"
+lineC :: ParseC ()
+lineC = do
+  splitIgnore $ break (== '\n')
+  stringC "\n" <|> return ()
+
+-- | Consume and measure the indentation.
+-- >>> testC "  hello" indentC
+-- "  "
+-- 2
+-- "hello"
+indentC :: ParseC Int
+indentC = splitXform splitWhitespace length
 
 
 parseCabal :: String -> Cabal
-parseCabal = fromJust . execWriterT . go . map splitIndent . lines
+parseCabal = fromJust . execWriterT . execStateT cabal
   where
-    go :: [(Int, ParseC String)] -> ParseC ()
-    go [] = return ()
-    go ((i, get_line):xs) = do
-        line <- get_line
-        build_depends i line xs <|> one_line line xs
+    cabal :: ParseC ()
+    cabal = eofC <|> ((build_depends <|> lineC) >> cabal)
     
-    one_line :: String -> [(Int, ParseC String)] -> ParseC ()
-    one_line s xs = do
-        stringC s
-        stringC "\n"
-        go xs
+    build_depends :: ParseC ()
+    build_depends = do
+        i <- indentC
+        stringC "build-depends"
+        whitespaceC
+        stringC ":"
+        dependency i
     
-    build_depends :: Int -> String -> [(Int, ParseC String)] -> ParseC ()
-    build_depends i s xs = do
-        s <- "build-depends" `isPrefixOfC` s
-        s <- dropWhitespaceC s
-        s <- ":" `isPrefixOfC` s
-        s <- dropWhitespaceC s
-        let (cs, xs') = splitUnindent i xs
-        dependencies s cs
-        go xs'
+    dependency :: Int -> ParseC ()
+    dependency i = do
+        spacing i
+        dependencyC
+        (spacing i >> stringC "," >> dependency i) <|> eolC
     
-    dependencies :: String -> [ParseC String] -> ParseC ()
-    dependencies "" [] = empty
-    dependencies "" (c:cs) = do
-        stringC "\n"
-        s <- c
-        dependencies s cs
-    dependencies s cs = do
-        let (s1, s2) = break (== ',') s
-        dependencyC s1
-        comma s2 cs
-    
-    comma :: String -> [ParseC String] -> ParseC ()
-    comma "" [] = stringC "\n"
-    comma "" (c:cs) = do
-        stringC "\n"
-        s <- c
-        comma s cs
-    comma s cs = do
-        s <- "," `isPrefixOfC` s
-        s <- dropWhitespaceC s
-        dependencies s cs
+    -- whitespace, including newlines, but stay withing the block of
+    -- data which is indented by more than i characters.
+    spacing :: Int -> ParseC ()
+    spacing i = do
+        whitespaceC
+        go <|> return ()
+      where
+        go = do
+            eolC
+            j <- indentC
+            go <|> guard (j > i)
 
 
 -- like readFile, but without the lazy IO
