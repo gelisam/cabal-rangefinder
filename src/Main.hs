1e124fec9797283b3382e18a59dcb5519d4757cd
Samuel Gélineau <gelisam+github@gmail.com>
2013-12-29 23:05:43 -0500

    validating that the cabal file builds fine

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,14 @@
 module Main where
 
 -- import Control.Arrow
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
+import Data.Maybe
 -- import Distribution.Text
 import System.Environment
+import System.Exit
+import System.Process
 
 import CabalFile
 import VersionFile
@@ -28,6 +34,25 @@ binary_search d mxs = do
     (mxs1, mx:mxs2) = splitAt i mxs
 
 
+-- | Does the given computation run until the end?
+succeeds :: MaybeT IO () -> IO Bool
+succeeds = fmap isJust . runMaybeT
+
+-- | Does the given command-line program succeed?
+run :: String -> MaybeT IO ()
+run cmd = do
+    exitCode <- lift $ system cmd
+    when (exitCode /= ExitSuccess) $ do
+      fail "command failed"
+
+-- | With the given Cabal file, does the project build?
+build :: FilePath -> Cabal -> MaybeT IO ()
+build cabal_file cabal = do
+    lift $ writeCabal cabal_file cabal
+    run "cabal-dev install-deps"
+    run "cabal-dev build"
+
+
 getCabalPath :: IO FilePath
 getCabalPath = do
     [cabal_file] <- getArgs
@@ -39,6 +64,10 @@ main = do
     cabal <- readCabal cabal_file
     -- mapM_ print cabal
     
+    untouchedOk <- succeeds $ build cabal_file cabal
+    when (not untouchedOk) $ do
+      error "You should at least start with a working .cabal file."
+    
     versionMap <- readVersionMap
     -- mapM_ print versionMap
     
@@ -48,4 +77,6 @@ main = do
     let our_versions = versionMap `restricted_to` our_packages
     -- mapM_ print $ map (display *** map display) our_versions
     
-    writeCabal cabal_file cabal
+    finalOk <- succeeds $ build cabal_file cabal
+    when (not finalOk) $ do
+      error "I spent all this time building the perfect .cabal file, and it doesn't even run :("
