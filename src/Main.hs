d55224dbf360f375f8ead6aadd0e31f1bb99264e
Samuel Gélineau <gelisam+github@gmail.com>
2014-01-04 19:16:17 -0500

    find version range (lower-bound and super verbose)

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,15 @@
 {-# LANGUAGE ImplicitParams #-}
 module Main where
 
--- import Control.Arrow
+import Control.Arrow
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
 import Distribution.Package
--- import Distribution.Text
+import Distribution.Text
 import Distribution.Version
 import System.Environment
+import Text.Printf
 
 import CabalFile
 import VersionFile
@@ -22,23 +23,37 @@ build_with_cabal :: (?cabal_file :: FilePath) => Cabal -> MaybeIO
 build_with_cabal cabal = do
     lift $ writeCabal ?cabal_file cabal
     run "cabal-dev install-deps"
+    run "cabal-dev configure"
     run "cabal-dev build"
+    lift $ putStrLn "cabal-rangefinder: OK"
 
 -- | How about with the default Cabal plus a specific version constrain?
 build_with_version :: (?cabal_file :: FilePath, ?cabal :: Cabal)
                    => PackageName -> Version
                    -> MaybeIO
-build_with_version p v = build_with_cabal cabal'
+build_with_version p v = do
+    lift header
+    build_with_cabal cabal'
   where
+    header :: IO ()
+    header = printf "cabal-rangefinder: trying %s %s\n" (display p) (display v)
+    
     cabal' = ?cabal // [(p, thisVersion v)]
 
 -- | Same, but with the type expected by binary_search.
 builds_with_version :: (?cabal_file :: FilePath, ?cabal :: Cabal)
                     => PackageName -> Version
-                    -> IO (Maybe Version)
+                    -> IO (Maybe [(PackageName, VersionRange)])
 builds_with_version p v = runMaybeT
-                        $ fmap (const v)
+                        $ fmap (const [(p, orLaterVersion v)])
                         $ build_with_version p v
+
+-- | Assuming the latest version works, how old can we go back?
+find_first_version :: (?cabal_file :: FilePath, ?cabal :: Cabal)
+                   => PackageName -> [Version]
+                   -> IO [(PackageName, VersionRange)]
+find_first_version p = binary_search []
+                     . map (builds_with_version p)
 
 
 getCabalPath :: IO FilePath
@@ -66,8 +81,22 @@ main = do
     -- mapM_ print our_packages
     
     let our_versions = versionMap `restricted_to` our_packages
-    -- mapM_ print $ map (display *** map display) our_versions
+    mapM_ print $ map (display *** map display) our_versions
     
-    finalOk <- succeeds $ build_with_cabal cabal
+    assignmentss <- forM our_versions $ \(p, vs) -> do
+      printf "cabal-rangefinder: exploring %s\n" (display p) :: IO ()
+      assignment <- find_first_version p vs
+      when (null assignment) $ do
+        printf "cabal-rangefinder: no good version for %s!\n" (display p) :: IO ()
+      forM_ assignment $ \(p', v) ->
+        printf "cabal-rangefinder: picked %s %s\n" (display p') (display v) :: IO ()
+      return assignment
+    print assignmentss
+    let assignments = concat assignmentss
+    -- assignments <- msum $ map (uncurry find_first_version) our_versions
+    print assignments
+    
+    let final_cabal = cabal // assignments
+    finalOk <- succeeds $ build_with_cabal final_cabal
     when (not finalOk) $ do
       error "I spent all this time building the perfect .cabal file, and it doesn't even run :("
